10f878a3a1a636372005dd5c360f4ff60f8e9ff9
Diego <diegopereiracalcada@gmail.com>
2019-04-20 01:45:49 -0300

    ADDED - Autenticação pelo Firebase

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, StyleSheet, Button} from  'react-native';
+import {View, StyleSheet, Button, Text, ActivityIndicator} from  'react-native';
 import FormRow from '../components/FormRow';
 import { TextInput } from 'react-native-gesture-handler';
+import firebase from 'firebase';
 
 export default class LoginPage extends React.Component{
     constructor(props){
@@ -9,7 +10,34 @@ export default class LoginPage extends React.Component{
 
         this.state = {
             mail : '',
-            pwd : ''
+            pwd : '',
+            isLoading : false,
+            msg : ''
+        }
+    }
+
+    componentDidMount(){
+        const config = {
+            apiKey: "AIzaSyD57HsBRmAcFZ-3nBrni7wgFiwWyQlu4fw",
+            authDomain: "seriesapp-d8186.firebaseapp.com",
+            databaseURL: "https://seriesapp-d8186.firebaseio.com",
+            projectId: "seriesapp-d8186",
+            storageBucket: "seriesapp-d8186.appspot.com",
+            messagingSenderId: "760606984784"
+        };
+        firebase.initializeApp(config);
+
+        
+    }
+
+    getMsgByErrorCode(errorCode){
+        switch(errorCode){
+            case 'auth/wrong-password':
+                return 'Senha incorreta';
+            case 'auth/user-not-found':
+                return 'Usuário não encontrado';
+            default:
+                return 'Erro desconhecido';
         }
     }
 
@@ -19,9 +47,45 @@ export default class LoginPage extends React.Component{
         });
     }
 
+    renderMsg(){
+        const { msg } = this.state;
+        if(!msg)
+        return null;
+
+        return (
+            <View>
+                <Text>{msg}</Text>
+            </View>
+        )
+    }
+
     tryLogin(){
-        console.log("Try login invoked");
-        console.log(this.state);
+        console.log("Tentando autenticar...");
+        this.setState({isLoading : true});
+        const { mail, pwd } = this.state;
+        setTimeout(()=>{
+            firebase
+            .auth()
+            .signInWithEmailAndPassword(mail, pwd)
+            .then((user) => {
+                this.setState({msg : 'Sucesso!'});
+            })
+            .catch((error)=>{
+                this.setState({msg : getMsgByErrorCode(error.code)});
+            })
+            .then(() => {
+                this.setState({isLoading : false});
+            });
+        }, 2000)
+    }
+
+    renderButton(){
+        if(this.state.isLoading){
+            return <ActivityIndicator />
+        }
+        return <Button 
+                    title="Entrar" onPress = {() => this.tryLogin()} />
+
     }
 
     render(){
@@ -44,7 +108,8 @@ export default class LoginPage extends React.Component{
                             onChangeText={newValue => this.onChangeHandler("pwd", newValue)}
                     />
                 </FormRow>
-                <Button title="Entrar" onPress = {() => this.tryLogin()} />
+                { this.renderButton() }
+                { renderMsg() }    
             </View>
         );
     }
